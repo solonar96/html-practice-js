090cb3aa80ad0731ad068c0e1a384dbb61fec409
Dmytro Solonar <solonar96@gmail.com>
2022-08-18 15:38:05 +0300

    Added jquery slider.

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg"></button>'
+    });
+  });
+  
