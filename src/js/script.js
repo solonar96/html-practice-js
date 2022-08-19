1c489babee8ff0ada814d8a98fa3c91fd9e01966
Dmytro Solonar <solonar96@gmail.com>
2022-08-19 18:18:07 +0300

    Added optimization for slider, added catalog section, added first catalog item.

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,21 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
   });
   
